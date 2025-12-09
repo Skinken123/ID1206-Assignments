515c8ea268e0ac361d37e91d371944d83d465a19
Alexander Nordgren <151035630+alnordg@users.noreply.github.com>
2025-12-09 13:33:10 +0100

    main

---
+++ Lab3_Group8_programming/Q[6]/q6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>  
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <N>\n", argv[0]);
+        return 1;
+    }
+
+
+
+
+    
+}
