5f1b8ae3c83a1873110080cba2204319caffb7cd
Skinken123 <guma.moritz@gmail.com>
2025-12-09 15:40:23 +0100

    q7 in progress

--- Lab3_Group8_programming/Q[7]/q7.c
+++ Lab3_Group8_programming/Q[7]/q7.c
@@ -4,17 +4,29 @@
 #include <sys/mman.h>
 #include <time.h>
 
-int main(int argc, char** argv){
+int main(int argc, char *argv[]){
+    if (argc != 3) {
+        printf("Usage: ./%s <num_pages> <option 1/option 2>\n", argv[0]);
+        return 0;
+    }
+
     time_t start,end;
     int num_pages = atoi(argv[1]);
-    int page_size = getpagesize ();
+    int option = atoi(argv[2]);
+    int page_size = getpagesize();
+    int allocation_size = num_pages * page_size;
 
     printf("Allocating %d pages of %d bytes \n", num_pages, page_size);
     char *addr;
     // @Add the start of Timer here
     // Option 1: @Add mmap below to allocate num_pages anonymous pages
+    if (option == 1) {
+        addr = mmap(NULL, allocation_size);
+    }
     // Option 2: @Add mmap below to allocate num_pages anonymous pages but using "huge" pages
-    //addr = (char*) mmap( ... );
+    if (option == 2) {
+        addr = (char*) mmap(NULL);
+    }
 
     if (addr == MAP_FAILED) {
         perror("mmap");
@@ -34,6 +46,6 @@ int main(int argc, char** argv){
         printf("%c ", addr[i*page_size]);
     }
     printf("\n");
-    
+
     munmap(addr, page_size*num_pages);
 }
